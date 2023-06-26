75af5a4c7603e9a3fc4073a31e20fe403d5a3128
Aleksandr Krepky <aleksandr.krepky@x5.ru>
2023-06-26 09:43:38 +0300

    Создано приложение Contingent на spring

--- contingent-stubs/src/commonMain/kotlin/ContStudStubPersons.kt
+++ contingent-stubs/src/commonMain/kotlin/ContStudStubPersons.kt
@@ -8,17 +8,11 @@ object ContStudStubPersons {
         get() = ContStudent(
             id = ContStudentId(777),
             fio = "Иванов Иван Иванович",
-/*            description = "Требуется болт 100x5 с шистигранной шляпкой",
-            ownerId = MkplUserId("user-1"),
-            adType = MkplDealSide.DEMAND,
-            visibility = MkplVisibility.VISIBLE_PUBLIC,
-            permissionsClient = mutableSetOf(
-                MkplAdPermissionClient.READ,
-                MkplAdPermissionClient.UPDATE,
-                MkplAdPermissionClient.DELETE,
-                MkplAdPermissionClient.MAKE_VISIBLE_PUBLIC,
-                MkplAdPermissionClient.MAKE_VISIBLE_GROUP,
-                MkplAdPermissionClient.MAKE_VISIBLE_OWNER,
-            )*/
+            sex = ContStudentSex.M,
+            semester = 2,
+            eduYear = 2023,
+            specialityId = 3,
+            facultyId = 2,
+            groupNum = "102-л",
         )
 }
