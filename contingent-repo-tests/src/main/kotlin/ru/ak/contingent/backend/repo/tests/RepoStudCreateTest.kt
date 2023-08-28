44787d9b69094e1a5cefca9ce5954b930d8bd6bd
Aleksandr Krepky <aleksandr.krepky@x5.ru>
2023-08-28 17:33:30 +0300

    Реализован репозитарий в PostgreSQL

--- contingent-repo-tests/src/main/kotlin/ru/ak/contingent/backend/repo/tests/RepoStudCreateTest.kt
+++ contingent-repo-tests/src/main/kotlin/ru/ak/contingent/backend/repo/tests/RepoStudCreateTest.kt
@@ -3,6 +3,7 @@ package ru.ak.contingent.backend.repo.tests
 import ru.ak.contingent.common.models.ContStudent
 import ru.ak.contingent.common.models.ContStudentId
 import ru.ak.contingent.common.models.ContStudentLock
+import ru.ak.contingent.common.models.ContStudentSex
 import ru.ak.contingent.common.repo.DbStudRequest
 import ru.ak.contingent.common.repo.IStudRepository
 import kotlin.test.Test
@@ -15,10 +16,12 @@ abstract class RepoStudCreateTest {
 
     private val createObj = ContStudent(
         fio = "Иванов Иван Иванович",
-//        description = "create object description",
-//        ownerId = MkplUserId("owner-123"),
-//        visibility = MkplVisibility.VISIBLE_TO_GROUP,
-//        adType = MkplDealSide.SUPPLY,
+        sex = ContStudentSex.M,
+        semester = 2,
+        eduYear = 2023,
+        specialityId = 3,
+        facultyId = 2,
+        groupNum = "102-л",
     )
 
     @Test
@@ -27,11 +30,12 @@ abstract class RepoStudCreateTest {
         val expected = createObj.copy(id = result.data?.id ?: ContStudentId.NONE)
         assertEquals(true, result.isSuccess)
         assertEquals(expected.fio, result.data?.fio)
-//        assertEquals(expected.description, result.data?.description)
-//        assertEquals(expected.adType, result.data?.adType)
-//        assertNotEquals(MkplAdId.NONE, result.data?.id)
-//        assertEquals(emptyList(), result.errors)
-//        assertEquals(lockNew, result.data?.lock)
+        assertEquals(expected.sex, result.data?.sex)
+        assertEquals(expected.semester, result.data?.semester)
+        assertEquals(expected.eduYear, result.data?.eduYear)
+        assertEquals(expected.specialityId, result.data?.specialityId)
+        assertEquals(expected.facultyId, result.data?.facultyId)
+        assertEquals(expected.groupNum, result.data?.groupNum)
     }
 
     companion object : BaseInitStuds("create") {
--- contingent-repo-tests/src/main/kotlin/ru/ak/contingent/backend/repo/tests/RepoStudUpdateTest.kt
+++ contingent-repo-tests/src/main/kotlin/ru/ak/contingent/backend/repo/tests/RepoStudUpdateTest.kt
@@ -3,48 +3,48 @@ package ru.ak.contingent.backend.repo.tests
 import ru.ak.contingent.common.models.ContStudent
 import ru.ak.contingent.common.models.ContStudentId
 import ru.ak.contingent.common.models.ContStudentLock
+import ru.ak.contingent.common.models.ContStudentSex
 import ru.ak.contingent.common.repo.DbStudRequest
 import ru.ak.contingent.common.repo.IStudRepository
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-
 abstract class RepoStudUpdateTest {
     abstract val repo: IStudRepository
     protected open val updateSucc = initObjects[0]
     protected open val updateConc = initObjects[1]
-    protected val updateIdNotFound = ContStudentId(-1)
-    protected val lockBad = ContStudentLock("20000000-0000-0000-0000-000000000009")
+    private val updateIdNotFound = ContStudentId(-1)
+    private val lockBad = ContStudentLock("20000000-0000-0000-0000-000000000009")
     protected val lockNew = ContStudentLock("20000000-0000-0000-0000-000000000002")
 
     private val reqUpdateSucc by lazy {
         ContStudent(
             id = updateSucc.id,
-            fio = "update object",
-//            description = "update object description",
-//            ownerId = MkplUserId("owner-123"),
-//            visibility = MkplVisibility.VISIBLE_TO_GROUP,
-//            adType = MkplDealSide.SUPPLY,
+            fio = "update student",
+            sex = ContStudentSex.M,
+            semester = 2,
+            eduYear = 2023,
+            specialityId = 3,
+            facultyId = 2,
+            groupNum = "102-л",
             lock = initObjects.first().lock,
         )
     }
     private val reqUpdateNotFound = ContStudent(
         id = updateIdNotFound,
-        fio = "update object not found",
-//        description = "update object not found description",
-//        ownerId = MkplUserId("owner-123"),
-//        visibility = MkplVisibility.VISIBLE_TO_GROUP,
-//        adType = MkplDealSide.SUPPLY,
+        fio = "update student not found",
+        sex = ContStudentSex.M,
+        semester = 2,
+        eduYear = 2023,
+        specialityId = 3,
+        facultyId = 2,
+        groupNum = "102-л",
         lock = initObjects.first().lock,
     )
     private val reqUpdateConc by lazy {
         ContStudent(
             id = updateConc.id,
-            fio = "update object not found",
-//            description = "update object not found description",
-//            ownerId = MkplUserId("owner-123"),
-//            visibility = MkplVisibility.VISIBLE_TO_GROUP,
-//            adType = MkplDealSide.SUPPLY,
+            fio = "update student not found",
             lock = lockBad,
         )
     }
@@ -55,8 +55,12 @@ abstract class RepoStudUpdateTest {
         assertEquals(true, result.isSuccess)
         assertEquals(reqUpdateSucc.id, result.data?.id)
         assertEquals(reqUpdateSucc.fio, result.data?.fio)
-//        assertEquals(reqUpdateSucc.description, result.data?.description)
-//        assertEquals(reqUpdateSucc.adType, result.data?.adType)
+        assertEquals(reqUpdateSucc.sex, result.data?.sex)
+        assertEquals(reqUpdateSucc.semester, result.data?.semester)
+        assertEquals(reqUpdateSucc.eduYear, result.data?.eduYear)
+        assertEquals(reqUpdateSucc.specialityId, result.data?.specialityId)
+        assertEquals(reqUpdateSucc.facultyId, result.data?.facultyId)
+        assertEquals(reqUpdateSucc.groupNum, result.data?.groupNum)
         assertEquals(emptyList(), result.errors)
     }
 
